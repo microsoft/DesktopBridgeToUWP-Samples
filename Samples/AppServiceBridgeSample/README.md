8b09d8726108aeb2386d85c621a47b106703f496
Vladimir <vladimp@microsoft.com>
2016-05-09 19:57:06 -0700

    Added App Service bridge sample

---
+++ Samples/AppServiceBridgeSample/README.md
@@ -0,0 +1,21 @@
+﻿# AppService Bridge Sample
+
+This sample demonstrates how you can create a package that includes both: win32 and UWP app and communicate between the two via AppService.
+It also shows you how to launch a win32 full trust proccess from a UWP via the new "fullTrustProcess" extension.
+By adding a UWP to your package you can enhance your app with new UWP features that were previously not available to desktop apps such as: background tasks, app services and UWP XAML UI frontend.
+Over time, gradually migrating  existing Win32/.NET code to move functionality into the app container. 
+Running your code in the app container benefits the security of your user’s machine, and at the end of the migration results in a UWP app that can run on PCs, Phones, IoT devices, Xbox One and HoloLens.
+
+Build/Deploy and Run the sample
+-------------------------------
+
+ - Debloy the solution by selecting **Build** \> **Deploy solution**.
+ - Make sure the FullTrustProcess.exe was copied to the Appx folder - UWP\bin\x64\Release\AppX if not rebuild the solution or copy it manually.
+ - Press F5 to run!
+ - Launch the Full trust process by pressing the "Launch FullTrust Process" button in the UWA.
+ - Send requests from the UWA and see the responses.
+
+Notes
+------
+
+- You can learn more about App Services [here] (https://msdn.microsoft.com/library/windows/apps/xaml/windows.applicationmodel.appservice.aspx)
---
+++ Samples/AppServiceBridgeSample/cs/UWP/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.ApplicationModel.AppService;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Loads XAML components 
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>MessageToSend
+        /// Launches the full trust process via the new "fullTrustProcess" extenstion
+        /// </summary>
+        private async void LaunchConsoleProcess_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.ApplicationModel.FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+        }
+
+        /// <summary>
+        /// Sends message to the full trust process and receives a response back
+        /// </summary>
+        private async void SendRequest_Click(object sender, RoutedEventArgs e)
+        {
+            ValueSet valueSet = new ValueSet();
+            valueSet.Add("request", MessageToSend.Text);
+
+            AppServiceResponse response = await App.Connection.SendMessageAsync(valueSet);
+            MessageRecevied.Text = "Received response: " + response.Message["response"] as string;
+        }
+    }
+}
