08c51540d6c0dfbc067bec4880963cbf5a3f5d39
Pete <peterfar@microsoft.com>
2016-06-02 16:23:02 -0700

    Add files via upload

---
+++ Samples/DesktopToUWPPackagingProjectSample/WpfUWP/WpfUWP/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+
+namespace WpfUWP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void show(object sender, RoutedEventArgs e)
+        {
+            showit();
+        }
+
+        private void showit()
+        {
+            XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text01);
+
+            XmlNodeList textNodes = tileXml.GetElementsByTagName("text");
+            textNodes[0].InnerText = string.Format("Welcome to DesktopToUWP !");
+
+            TileNotification tileNotification = new TileNotification(tileXml);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+        }
+    }
+}
