ba5845c837ca4e8f22b515ffaa8b52f4fa6c62b3
Vladimir <vladimp@microsoft.com>
2016-06-09 22:50:24 -0700

    Added a set of samples for the blog post - Journey across the bridge

---
+++ Samples/JourneyAcrossTheBridge/DesktopBridgeDemo - Step1/MyDesktopApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        bool initialized = false; 
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in comboBox1.Items)
+            {
+                if (item.ToString() == Program.CurrentStatus)
+                {
+                    comboBox1.SelectedItem = item;
+                    initialized = true;
+                    break;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!initialized) return;
+            Program.RegKey.SetValue("Status", comboBox1.SelectedItem);
+        }
+    }
+}
