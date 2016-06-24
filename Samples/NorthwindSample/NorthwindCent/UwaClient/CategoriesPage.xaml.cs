7129a837f578ad0f12b3730526313caf99dcb12c
Rick Li <rickli@microsfot.com>
2016-06-24 14:37:13 -0700

    Add Northwind Sample

---
+++ Samples/NorthwindSample/NorthwindCent/UwaClient/CategoriesPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using NorthwindCent.UwaClient.Common;
+
+namespace NorthwindCent.UwaClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CategoriesPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ViewModel viewModelObj;
+
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        public ObservableDictionary DefaultViewModel { get { return this.defaultViewModel; } }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper { get { return this.navigationHelper; } }
+
+        public CategoriesPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += NavigationHelper_LoadState;
+
+            // Register to receive event when connection to desktop server has been built
+            (App.Current as App).AppServiceConnected += CategoriesPage_AppServiceConntected;
+        }
+
+        private async void CategoriesPage_AppServiceConntected(object sender, EventArgs e)
+        {
+            await viewModelObj.LoadCategoriesAsync();
+        }
+
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            viewModelObj = ViewModel.Instance;
+            await viewModelObj.LoadCategoriesAsync();
+
+            DefaultViewModel["Items"] = viewModelObj.Categories;
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void CategoriesGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem != null)
+            {
+                var category = ((Category)e.ClickedItem);
+                Frame.Navigate(typeof(ProductsPage), category);
+            }
+        }
+    }
+}
---
+++ Samples/NorthwindSample/NorthwindCent/UwaClient/ProductsPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+
+using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using NorthwindCent.UwaClient.Common;
+
+namespace NorthwindCent.UwaClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ProductsPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ViewModel viewModelObj;
+
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        public ObservableDictionary DefaultViewModel { get { return this.defaultViewModel; } }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public ProductsPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += NavigationHelper_LoadState;
+        }
+
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            // TODO: Assign a bindable collection of items to this.DefaultViewModel["Items"]
+            viewModelObj = ViewModel.Instance;
+            await viewModelObj.LoadProductsForCategoryAsync(((Category)e.NavigationParameter));
+            DefaultViewModel["Items"] = viewModelObj;
+            DefaultViewModel["Order"] = viewModelObj;
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void ProductsGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem != null)
+            {
+                var product = ((Product)e.ClickedItem);
+                viewModelObj.Order.Add(product);
+                viewModelObj.UpdateTotal();
+            }
+        }
+
+        private void OrdersGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem != null)
+            {
+                var product = ((Product)e.ClickedItem);
+                viewModelObj.Order.Remove(product);
+                viewModelObj.UpdateTotal();
+            }
+        }
+
+        private void CheckOut_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Instance.CheckOut();
+        }
+    }
+}
