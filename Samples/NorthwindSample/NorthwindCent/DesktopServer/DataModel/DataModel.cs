e76e2ee83025d01107060ab6f5574e0f6931fa62
Rick Li <coolrick@gmail.com>
2016-07-05 15:15:34 -0700

    Add Northwind Sample

---
+++ Samples/NorthwindSample/NorthwindCent/DesktopServer/DataModel/DataModel.cs
@@ -0,0 +1,40 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+namespace NorthwindCent.DataModel
+{
+    /// <summary>
+    /// Object model for Category
+    /// </summary>
+    public struct Category
+    {
+        public int ID;
+        public string Name;
+        public string Description;
+    }
+
+    /// <summary>
+    /// Object model for Product
+    /// </summary>
+    public struct Product
+    {
+        public int ID;
+        public string Name;
+        public string QuantityPerUnit;
+        public float UnitPrice;
+        public short UnitsInStock;
+        public short UnitsOnOrder;
+        public short ReorderLevel;
+        public bool Discontinued;
+        public int SupplierID;
+        public int CategoryID;
+    }
+}
