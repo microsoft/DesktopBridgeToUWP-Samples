31c2ea52eaa9d06daf38efd98fa07285964c5a81
Vladimir <vladimp@microsoft.com>
2016-09-20 17:23:32 -0700

    Added onTaskCanceled handler

--- Samples/AppServiceBridgeSample/cs/UWP/App.xaml.cs
+++ Samples/AppServiceBridgeSample/cs/UWP/App.xaml.cs
@@ -49,8 +49,19 @@ namespace SDKTemplate
             if (args.TaskInstance.TriggerDetails is AppServiceTriggerDetails)
             {
                 appServiceDeferral = args.TaskInstance.GetDeferral();
+                args.TaskInstance.Canceled += OnTaskCanceled; // Associate a cancellation handler with the background task.
+
                 AppServiceTriggerDetails details = args.TaskInstance.TriggerDetails as AppServiceTriggerDetails;
                 Connection = details.AppServiceConnection;
+            }
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.appServiceDeferral != null)
+            {
+                // Complete the service deferral.
+                this.appServiceDeferral.Complete(); 
             }
         }
 
