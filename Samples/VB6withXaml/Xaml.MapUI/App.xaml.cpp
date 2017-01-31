c643f295caf9ee2953b002a767a8a1e25244aa25
Vladimir Postel <vladimp@microsoft.com>
2017-01-30 17:35:53 -0800

    Added Copyright to the code files and ReadMe file give guidance to the developers

--- Samples/VB6withXaml/Xaml.MapUI/App.xaml.cpp
+++ Samples/VB6withXaml/Xaml.MapUI/App.xaml.cpp
@@ -1,7 +1,13 @@
-﻿//
-// App.xaml.cpp
-// Implementation of the App class.
+﻿//*********************************************************
 //
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
 
 #include "pch.h"
 #include <iostream> 
