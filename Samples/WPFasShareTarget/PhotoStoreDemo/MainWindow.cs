4d36fdbce9a12e92e4ca9d3b7d9c0137b7233fef
Vladimir Postel <vladimp@microsoft.com>
2017-01-31 18:46:43 -0800

    Added CopyRights label to the source and a ReadMe file

--- Samples/WPFasShareTarget/PhotoStoreDemo/MainWindow.cs
+++ Samples/WPFasShareTarget/PhotoStoreDemo/MainWindow.cs
@@ -1,4 +1,15 @@
-﻿// // Copyright (c) Microsoft. All rights reserved.
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+// // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
