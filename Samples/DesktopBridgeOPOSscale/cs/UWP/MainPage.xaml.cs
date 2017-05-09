eaabb8dd940fc58000d0a8fd7d10cd98995da476
Marcus Duarte Breda <breda.marcus@gmail.com>
2017-05-09 10:38:31 -0700

    fix code review suggestion

--- Samples/DesktopBridgeOPOSscale/cs/UWP/MainPage.xaml.cs
+++ Samples/DesktopBridgeOPOSscale/cs/UWP/MainPage.xaml.cs
@@ -21,9 +21,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace SDKTemplate
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
         public static MainPage Current;
